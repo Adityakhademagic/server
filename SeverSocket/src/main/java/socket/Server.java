1f469c22b61db85bc83ff6f8a2a46cc0ab4c60aa
Adityakhademagic <104549315+Adityakhademagic@users.noreply.github.com>
2022-05-16 11:14:56 +0530

    Update Server.java

--- SeverSocket/src/main/java/socket/Server.java
+++ SeverSocket/src/main/java/socket/Server.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    private Socket socket = null;
+    private Socket socket;
 
     public Server(int port) throws IOException {
 
